550ebf71ba92827e8e48258745efd92021da0d22
Abhinand T S <abhinandts116@gmail.com>
2025-08-01 18:21:37 +0530

    Folder structure updated

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,31 @@
-
-import express, {Application} from "express";
+import express, { Application } from "express";
 import dotenv from 'dotenv'
+import mongoose from "mongoose";
+
 
 dotenv.config();
 
-const app:Application = express()
+const app: Application = express()
 const PORT = process.env.PORT
 
-app.listen(PORT, () => console.log("server started...."))
+app.use(express.json());
+
+const connectDB = async (): Promise<void> => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI as string)
+        console.log("connected")
+    } catch (error) {
+        console.log("mongo db connection failed")
+        process.exit(1);
+    }
+}
+
+const startServer = async (): Promise<void> => {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log("running..")
+    })
+}
+
+startServer();
